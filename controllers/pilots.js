0b3abd5f4cc343fc04ab4fa896f559b3ad15171e
gucaobianco <gu.caobianco@gmail.com>
2022-01-17 20:57:24 -0300

    added root path validators

--- controllers/pilots.js
+++ controllers/pilots.js
@@ -3,18 +3,24 @@ const {isUserLoggedIn} = require("../validators/user-logged-in-validator");
 const {isTokenValid} = require("../validators/token-validator");
 
 module.exports = app => {
-    app.get("/pilots", isUserLoggedIn, isTokenValid, (req, res, next) => {
+    app.use("/pilots", isUserLoggedIn, isTokenValid, (req, res, next) => {
+        console.log("passed on router verification");
+        next();
+    })
+
+    app.get("/pilots", (req, res, next) => {
         const pilots = pilotService.findAll();
         res.send(pilots);
         return;
     });
 
-    app.get("/pilots/:id", isUserLoggedIn, isTokenValid, (req, res, next) => {
+    app.get("/pilots/:id", (req, res, next) => {
         const pilot = pilotService.findById(req.params.id);
         if(pilot){
             res.send(pilot);
             return;
         }
         res.status(404).send(`pilot id: ${typeof req.params.id} not found`);
+        return;
     });
 }
