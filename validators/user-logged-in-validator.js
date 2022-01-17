819a5a3be46c8a9e52556c9ca0383535c4fa51d6
gucaobianco <gu.caobianco@gmail.com>
2022-01-17 20:16:53 -0300

    short circuited login validator
    without return it calls next after sending a response

--- validators/user-logged-in-validator.js
+++ validators/user-logged-in-validator.js
@@ -1,6 +1,7 @@
 function isUserLoggedIn(req, res, next) {
     if (req.headers["authorization"] == null || req.headers["authorization"].trim().length < 1) {
         res.status(401).send("authorization is required");
+        return;
     }
     next();
 }
